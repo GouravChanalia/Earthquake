1a96715e91041b206aab7557532d63a3f5e3dc87
gouravchanalia <gourav1799.cse18@chitkara.edu.in>
2020-11-03 14:46:05 +0530

    JSON used to retrieve data from the API.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -20,6 +20,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
@@ -31,26 +35,7 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes = new ArrayList<>();
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2,2016"));
-        earthquakes.add(new Earthquake(6.1, "London", "July 20,2015"));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", "Nov 10,2014"));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3,2014"));
-        earthquakes.add(new Earthquake(2.8, "Moscow", "Jan 31,2013"));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "Aug 19,2012"));
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2,2016"));
-        earthquakes.add(new Earthquake(6.1, "London", "July 20,2015"));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", "Nov 10,2014"));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3,2014"));
-        earthquakes.add(new Earthquake(2.8, "Moscow", "Jan 31,2013"));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "Aug 19,2012"));
-        earthquakes.add(new Earthquake(7.2, "San Francisco", "Feb 2,2016"));
-        earthquakes.add(new Earthquake(6.1, "London", "July 20,2015"));
-        earthquakes.add(new Earthquake(3.9, "Tokyo", "Nov 10,2014"));
-        earthquakes.add(new Earthquake(5.4, "Mexico City", "May 3,2014"));
-        earthquakes.add(new Earthquake(2.8, "Moscow", "Jan 31,2013"));
-        earthquakes.add(new Earthquake(4.9, "Rio de Janeiro", "Aug 19,2012"));
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
